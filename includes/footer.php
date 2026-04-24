9716c516871ea03c200097b9d39a34d14841f3b8
Rahma <rahmamahamed.mahamud@elev.ga.ntig.se>
2026-04-24 10:55:52 +0200

    ändrade header.php jag hade fel länk för js, och annan fel var att jag först skrev filnamnet script.js men sedan glömde att ändra till det canvas-clock.js

--- includes/footer.php
+++ includes/footer.php
@@ -19,7 +19,5 @@
     </div>
 </div>
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-<script src="canvas-clock.js"></script>
 </body>
 </html>
